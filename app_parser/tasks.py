f9a1936a618d899dd0a763c5a3abdd10f3c749b1
dimafdrnk <dimawkfdrng@gmail.com>
2022-10-27 09:59:17 +0300

    Парсинг данных с хабр

--- app_parser/tasks.py
+++ app_parser/tasks.py
@@ -1,11 +1,9 @@
 from urllib.parse import urljoin
-from .models import News
 import  requests
 from bs4 import BeautifulSoup
 
 
 URLS = ['https://habr.com/ru/news/top/daily/']
-
 
 class Crawler:
     def __init__(self, urls):
@@ -26,23 +24,30 @@ class Pareser(Crawler):
     def __init__(self, soup):
         self.soup = soup
 
-    def get_data(self):
-        data = []
-        for url in self.soup:
-            received_data = self.soup[url].find('div', class_='tm-articles-list')
-            for i in received_data.find_all('article'):
-                data.append({
-                'title': i.find('h2').text,
-                'url': urljoin(url, i.find('a', class_='tm-article-snippet__title-link')['href'])})
+    def get_urls(self):
+        urls_article = []
+        for key in self.soup:
+            received_data = self.soup[key].find('div', class_='tm-articles-list')
+            for article in received_data.find_all('article'):
+                urls_article.append(urljoin(key, article.find('a', class_='tm-article-snippet__title-link')['href']))
+        return urls_article
 
-        return data
+    def article_data(self, urls_article):
+        article_data = []
+        soup = Crawler(urls_article).get_soup()
+        for key in soup:
+            article_data.append({
+                'title': soup[key].find('h1').text,
+                'image': str(soup[key].find('figure')),
+                'text': soup[key].find('div', id='post-content-body').text})
+
+        return article_data
 
 
-def save_in_base():
-    soup = Crawler(URLS).get_soup()
-    habr = Pareser(soup)
-    data = habr.get_data()
-    return data
 
-
+# soup = Crawler(URLS).get_soup()
+# habr = Pareser(soup)
+# data = habr.get_data()
+# d = habr.news_data(['https://habr.com/ru/news/t/695364/'])
+# print(data)
 
